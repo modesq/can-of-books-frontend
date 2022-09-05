a32a5f69352db686320cc34794ac64a2c918ad9e
mahmoud qunbor <modesq.99q@gmail.com>
2022-09-06 02:28:14 +0300

    another future problem i guess

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,6 +10,26 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  addBooks = (event) => {
+    event.preventDefault();
+
+    const obj = {
+      title: event.target.title.value,
+      description: event.target.description.value,
+      status: event.target.status.value
+    }
+
+    axios
+      .get(`http://localhost:3010/getBooks`, obj)
+      .then(result => {
+        this.setState({
+          books: result.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
 
   render() {
 
@@ -19,7 +40,19 @@ class BestBooks extends React.Component {
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          // <p>Book Carousel coming soon</p>
+          <div>
+            <h1>book System</h1>
+            {this.state.books.map(item => {
+              return (
+                <div>
+                  <h3>book name : {item.title} </h3>
+                  <p>book description : {item.description}</p>
+                  <h4>book state : {item.state}</h4>
+                </div>
+              )
+            })}
+          </div>
         ) : (
           <h3>No Books Found :(</h3>
         )}
