d7baf024f025e72659faaee285d34ed482cd4a15
mahmoud qunbor <modesq.99q@gmail.com>
2022-09-07 00:20:15 +0300

    lab 11 done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import Carousel from "react-bootstrap/Carousel";
+
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,27 +11,20 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
-  addBooks = (event) => {
-    event.preventDefault();
-
-    const obj = {
-      title: event.target.title.value,
-      description: event.target.description.value,
-      status: event.target.status.value
-    }
-
+  componentDidMount = () => {
     axios
-      .get(`http://localhost:3010/getBooks`, obj)
-      .then(result => {
+      .get("http://localhost:3000/getBooks")
+      .then((result) => {
         this.setState({
-          books: result.data
-        })
+          books: result.data,
+        });
+        // console.log(this.state.books);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+  };
+
 
   render() {
 
@@ -41,18 +36,26 @@ class BestBooks extends React.Component {
 
         {this.state.books.length ? (
           // <p>Book Carousel coming soon</p>
-          <div>
-            <h1>book System</h1>
-            {this.state.books.map(item => {
+          <Carousel>
+            {this.state.books.map((item) => {
               return (
-                <div>
-                  <h3>book name : {item.title} </h3>
-                  <p>book description : {item.description}</p>
-                  <h4>book state : {item.state}</h4>
-                </div>
-              )
-            })}
-          </div>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="https://images.unsplash.com/photo-1553095066-5014bc7b7f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbCUyMGJhY2tncm91bmR8ZW58MHx8MHx8&w=1000&q=80"
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                    <p>{item.status}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            }
+            )
+            }
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
